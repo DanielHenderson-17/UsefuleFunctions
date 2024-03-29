4e6c6208080ad841bf3962796c130eeedd2770bc
Daniel Henderson <djhindu17@gmail.com>
2024-03-29 12:00:00 -0500

    Add 04_generateUUID utility function

--- temp.js
+++ temp.js
@@ -1,4 +1,7 @@
-function calculateFibonacci(n) {
-  if (n <= 1) return n;
-  return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+function generateUUID() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
