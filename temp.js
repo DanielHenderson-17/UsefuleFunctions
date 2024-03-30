54c12dd543c200b99b033ee1a99d7ffe9a80f192
Daniel Henderson <djhindu17@gmail.com>
2024-03-30 12:00:00 -0500

    Add 05_shuffleArray utility function

--- temp.js
+++ temp.js
@@ -1,3 +1,7 @@
-function capitalizeWords(str) {
-  return str.replace(/\b\w/g, (char) => char.toUpperCase());
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
 }
