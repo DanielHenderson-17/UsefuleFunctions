f5ebd9c1ea411facbf0ccd66683e71509f6949ed
Daniel Henderson <djhindu17@gmail.com>
2025-02-16 13:00:00 -0600

    Implement title case formatter

--- utilityFunctions.js
+++ utilityFunctions.js
@@ -1,3 +1,8 @@
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
+
+
+function toTitleCase(str) {
+  return str.toLowerCase().replace(/\b\w/g, char => char.toUpperCase());
+}
