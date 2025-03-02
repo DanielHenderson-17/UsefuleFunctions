a4ffdbc087e7e88f74dc1d905c442beaed45e5d3
Daniel Henderson <djhindu17@gmail.com>
2025-03-02 12:00:00 -0600

    Add name initials extractor

--- utilityFunctions.js
+++ utilityFunctions.js
@@ -48,3 +48,7 @@ function mergeArrays(a, b) {
 function truncate(str, length) {
   return str.length > length ? str.substring(0, length) + '...' : str;
 }
+
+function getInitials(name) {
+  return name.split(' ').map(word => word[0].toUpperCase()).join('');
+}
