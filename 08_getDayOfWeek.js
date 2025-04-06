1a54e46a63d07a8f0d0978b83e94e4c440335c5a
Daniel Henderson <djhindu17@gmail.com>
2025-04-06 12:00:00 -0500

    Add 08_getDayOfWeek utility function

---
+++ 08_getDayOfWeek.js
@@ -0,0 +1,12 @@
+function getDayOfWeek(dateStr) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[new Date(dateStr).getDay()];
+}
