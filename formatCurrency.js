f11df60c850b5afde99f06e65a71f7df68f98beb
Daniel Henderson <djhindu17@gmail.com>
2025-07-21 09:30:00 -0500

    Add utility function to format numbers as USD currency

---
+++ formatCurrency.js
@@ -0,0 +1,9 @@
+// Formats number to USD currency string
+function formatCurrency(amount) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(amount);
+}
+
+module.exports = formatCurrency;
